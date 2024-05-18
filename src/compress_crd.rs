ec60e9b96aebb562d591412cfc6dbfff2bb3d22d
jjitch <junnosuke_08@icloud.com>
2024-05-19 02:03:32 +0900

    improve usability

--- src/compress_crd.rs
+++ src/compress_crd.rs
@@ -1,13 +1,12 @@
-pub struct CC<T: std::cmp::Ord> {
+#[derive(Debug)]
+pub struct CC<T: Ord> {
     m_i2c: Vec<T>,
     m_c2i: std::collections::BTreeMap<T, usize>,
 }
 
-impl<T: std::cmp::Ord + Clone> CC<T> {
-    pub fn new(v: &Vec<T>) -> Self {
-        let uncomp: Vec<_> = v
-            .iter()
-            .map(|v| v.clone())
+impl<T: Ord + Clone> CC<T> {
+    pub fn new(it: impl std::iter::Iterator<Item = T>) -> Self {
+        let uncomp: Vec<_> = it
             .collect::<std::collections::BTreeSet<T>>()
             .into_iter()
             .collect();
@@ -27,5 +26,14 @@ impl<T: std::cmp::Ord + Clone> CC<T> {
     }
     pub fn c2i(&self, c: &T) -> usize {
         *self.m_c2i.get(c).unwrap()
+    }
+    pub fn len(&self) -> usize {
+        self.m_c2i.len()
+    }
+}
+
+impl<Coord: Ord + Clone> std::iter::FromIterator<Coord> for CC<Coord> {
+    fn from_iter<T: IntoIterator<Item = Coord>>(iter: T) -> Self {
+        Self::new(iter.into_iter())
     }
 }
--- tests/compress_crd.rs
+++ tests/compress_crd.rs
@@ -3,7 +3,7 @@ mod compress_crd_test {
     #[test]
     fn compress_1() {
         let v = vec![2, 43, 2, 6, 23, 6];
-        let cc = CC::new(&v);
+        let cc = CC::new(v.into_iter());
         assert_eq!(cc.i2c(0), 2);
         assert_eq!(cc.i2c(1), 6);
         assert_eq!(cc.i2c(2), 23);
@@ -12,5 +12,25 @@ mod compress_crd_test {
         assert_eq!(cc.c2i(&6), 1);
         assert_eq!(cc.c2i(&23), 2);
         assert_eq!(cc.c2i(&43), 3);
+    }
+
+    #[test]
+    fn char_comp() {
+        let cc = CC::new('a'..='z');
+        assert_eq!(cc.c2i(&'a'), 0);
+        assert_eq!(cc.c2i(&'b'), 1);
+        assert_eq!(cc.c2i(&'c'), 2);
+        assert_eq!(cc.c2i(&'d'), 3);
+        assert_eq!(cc.c2i(&'z'), 25);
+    }
+
+    #[test]
+    fn from_iter() {
+        let cc = ('a'..='z').collect::<CC<char>>();
+        assert_eq!(cc.c2i(&'a'), 0);
+        assert_eq!(cc.c2i(&'b'), 1);
+        assert_eq!(cc.c2i(&'c'), 2);
+        assert_eq!(cc.c2i(&'d'), 3);
+        assert_eq!(cc.c2i(&'z'), 25);
     }
 }
