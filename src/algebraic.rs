0bea3df654b9b9c466c92084cd78fb305e7a4d7d
jjitch <junnosuke_08@icloud.com>
2024-06-13 00:22:24 +0900

    refine implmentation

--- src/algebraic.rs
+++ src/algebraic.rs
@@ -1,87 +1,204 @@
-pub trait Magma<T> {
-    fn operate(x: &T, y: &T) -> T;
+pub trait Monoid {
+    type Element;
+    fn operate(x: &Self::Element, y: &Self::Element) -> Self::Element;
+    fn identity() -> Self::Element;
 }
 
-pub trait Identity<T> {
-    fn identity() -> T;
+pub trait Group: Monoid {
+    fn inverse(x: &Self::Element) -> Self::Element;
 }
 
-pub trait Inverse<T> {
-    fn inverse(x: &T) -> T;
+pub trait Act: Monoid {
+    type Target;
+    fn act(x: &Self::Target, m: &Self::Element) -> Self::Target;
+    fn proportional(m: &Self::Element, n: usize) -> Self::Element;
 }
 
-pub trait Monoid<T>: Magma<T> + Identity<T> {}
-impl<T, M> Monoid<T> for M where M: Magma<T> + Identity<T> {}
+pub struct Add;
 
-pub trait Group<T>: Monoid<T> + Inverse<T> {}
-impl<T, G> Group<T> for G where G: Monoid<T> + Inverse<T> {}
+impl Monoid for Add {
+    type Element = i64;
+    fn identity() -> Self::Element {
+        0
+    }
+    fn operate(x: &Self::Element, y: &Self::Element) -> Self::Element {
+        x + y
+    }
+}
 
-pub mod mount {
+impl Group for Add {
+    fn inverse(x: &Self::Element) -> Self::Element {
+        -x
+    }
+}
 
-    use num::Integer;
+impl Act for Add {
+    type Target = i64;
+    fn act(m: &Self::Target, x: &Self::Element) -> Self::Target {
+        m + x
+    }
+    fn proportional(m: &Self::Element, n: usize) -> Self::Element {
+        m * n as i64
+    }
+}
 
-    use super::*;
+pub struct Min;
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+pub enum MinElm {
+    Finite(i64),
+    Infinite,
+}
 
-    #[derive(Debug)]
-    pub struct Add;
+impl std::fmt::Debug for MinElm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                &MinElm::Finite(x) => x.to_string(),
+                &MinElm::Infinite => "INF".to_string(),
+            }
+        )
+    }
+}
 
-    impl Magma<i64> for Add {
-        fn operate(x: &i64, y: &i64) -> i64 {
-            x + y
+impl From<i64> for MinElm {
+    fn from(value: i64) -> Self {
+        Self::Finite(value)
+    }
+}
+
+impl MinElm {
+    pub fn unwrap(&self) -> i64 {
+        match self {
+            &MinElm::Finite(i) => i,
+            &MinElm::Infinite => panic!("The content is INFINITE."),
         }
     }
+}
 
-    impl Identity<i64> for Add {
-        fn identity() -> i64 {
-            0
+impl Monoid for Min {
+    type Element = MinElm;
+    fn identity() -> Self::Element {
+        MinElm::Infinite
+    }
+    fn operate(x: &Self::Element, y: &Self::Element) -> Self::Element {
+        *x.min(y)
+    }
+}
+
+impl Act for Min {
+    type Target = i64;
+    fn act(x: &Self::Target, m: &Self::Element) -> Self::Target {
+        match *m {
+            MinElm::Finite(m) => (*x).min(m),
+            MinElm::Infinite => *x,
         }
     }
+    fn proportional(m: &Self::Element, _: usize) -> Self::Element {
+        *m
+    }
+}
 
-    impl Inverse<i64> for Add {
-        fn inverse(x: &i64) -> i64 {
-            -x
+pub struct Max;
+#[derive(PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+pub enum MaxElm {
+    NegInf,
+    Finite(i64),
+}
+
+impl std::fmt::Debug for MaxElm {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                &MaxElm::Finite(x) => x.to_string(),
+                &MaxElm::NegInf => "-INF".to_string(),
+            }
+        )
+    }
+}
+
+impl From<i64> for MaxElm {
+    fn from(value: i64) -> Self {
+        Self::Finite(value)
+    }
+}
+
+impl MaxElm {
+    pub fn unwrap(&self) -> i64 {
+        match self {
+            &MaxElm::Finite(i) => i,
+            &MaxElm::NegInf => panic!("The content is INFINITE."),
         }
     }
+}
 
-    #[derive(Debug)]
-    pub struct Gcd;
-    impl Magma<i64> for Gcd {
-        fn operate(x: &i64, y: &i64) -> i64 {
-            x.gcd(y)
+impl Monoid for Max {
+    type Element = MaxElm;
+    fn identity() -> Self::Element {
+        MaxElm::NegInf
+    }
+    fn operate(x: &Self::Element, y: &Self::Element) -> Self::Element {
+        *x.max(y)
+    }
+}
+
+impl Act for Max {
+    type Target = i64;
+    fn act(x: &Self::Target, m: &Self::Element) -> Self::Target {
+        match *m {
+            MaxElm::Finite(m) => (*x).max(m),
+            MaxElm::NegInf => *x,
         }
     }
+    fn proportional(m: &Self::Element, _: usize) -> Self::Element {
+        *m
+    }
+}
 
-    impl Identity<i64> for Gcd {
-        fn identity() -> i64 {
-            0
+pub struct Gcd;
+
+impl Monoid for Gcd {
+    type Element = i64;
+    fn identity() -> Self::Element {
+        0
+    }
+    fn operate(x: &Self::Element, y: &Self::Element) -> Self::Element {
+        if *y == 0 {
+            *x
+        } else {
+            Self::operate(y, &(x % y))
         }
     }
+}
 
-    #[derive(Debug)]
-    pub struct Max;
-    impl Magma<i64> for Max {
-        fn operate(x: &i64, y: &i64) -> i64 {
-            *x.max(y)
+pub struct Assign<T>(std::marker::PhantomData<fn() -> T>);
+
+impl<T: Clone> Monoid for Assign<T> {
+    type Element = Option<T>;
+    fn identity() -> Self::Element {
+        None
+    }
+    fn operate(x: &Self::Element, y: &Self::Element) -> Self::Element {
+        if y.is_some() {
+            y.clone()
+        } else {
+            x.clone()
         }
     }
+}
 
-    impl Identity<i64> for Max {
-        fn identity() -> i64 {
-            -(1i64 << 60)
+impl<T: Clone> Act for Assign<T> {
+    type Target = T;
+    fn act(x: &Self::Target, m: &Self::Element) -> Self::Target {
+        match m {
+            &Some(ref t) => t.clone(),
+            &None => x.clone(),
         }
     }
-
-    #[derive(Debug)]
-    pub struct Min;
-    impl Magma<i64> for Min {
-        fn operate(x: &i64, y: &i64) -> i64 {
-            *x.min(y)
-        }
-    }
-
-    impl Identity<i64> for Min {
-        fn identity() -> i64 {
-            1i64 << 60
-        }
+    fn proportional(m: &Self::Element, _: usize) -> Self::Element {
+        m.clone()
     }
 }
--- tests/algebraic.rs
+++ tests/algebraic.rs
@@ -3,34 +3,52 @@ mod algebraic_test {
     use snippet::algebraic::*;
     #[test]
     fn add() {
-        assert_eq!(mount::Add::identity(), 0);
-        assert_eq!(mount::Add::operate(&2, &3), 5);
-        assert_eq!(mount::Add::inverse(&234), -234);
+        assert_eq!(Add::identity(), 0);
+        assert_eq!(Add::operate(&2, &3), 5);
+        assert_eq!(Add::inverse(&234), -234);
     }
     #[test]
     fn gcd() {
-        assert_eq!(mount::Gcd::identity(), 0);
-        assert_eq!(mount::Gcd::operate(&117, &57), 3);
-        assert_eq!(
-            mount::Gcd::operate(&4541154, &mount::Gcd::identity()),
-            4541154
-        );
+        assert_eq!(Gcd::identity(), 0);
+        assert_eq!(Gcd::operate(&117, &57), 3);
+        assert_eq!(Gcd::operate(&4541154, &Gcd::identity()), 4541154);
     }
 
     #[test]
     fn max() {
-        assert_eq!(mount::Max::operate(&111111, &44), 111111);
         assert_eq!(
-            mount::Max::operate(&-144154, &mount::Max::identity()),
-            -144154
+            MaxElm::NegInf.partial_cmp(&MaxElm::Finite(0)),
+            Some(std::cmp::Ordering::Less)
+        );
+        assert_eq!(
+            MaxElm::Finite(0).partial_cmp(&Max::identity()),
+            Some(std::cmp::Ordering::Greater)
+        );
+        assert_eq!(
+            Max::operate(&MaxElm::Finite(111111), &MaxElm::Finite(44)),
+            MaxElm::Finite(111111)
+        );
+    }
+    #[test]
+    fn max2() {
+        assert_eq!(
+            Max::operate(&MaxElm::NegInf, &MaxElm::Finite(-144154)),
+            MaxElm::Finite(-144154)
+        );
+        assert_eq!(
+            Max::operate(&(-144154).into(), &Max::identity()),
+            (-144154).into()
         );
     }
     #[test]
     fn min() {
-        assert_eq!(mount::Min::operate(&111111, &44), 44);
         assert_eq!(
-            mount::Min::operate(&-144154, &mount::Min::identity()),
-            -144154
+            Min::operate(&MinElm::Finite(111111), &MinElm::Finite(44)),
+            MinElm::Finite(44)
+        );
+        assert_eq!(
+            Min::operate(&MinElm::Finite(-144154), &Min::identity()),
+            MinElm::Finite(-144154)
         );
     }
 }
